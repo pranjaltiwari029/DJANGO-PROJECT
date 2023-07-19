bfad2ab4001985d3540dcbe152fd8cb40480abf3
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-19 12:52:26 +0530

    Create models.py

---
+++ IMDB/imdb_api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class StreamPlatform(models.Model):
+    name=models.CharField(max_length=50)
+    about=models.CharField(max_length=100)
+    website=models.URLField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class WatchList(models.model):
+    
+    title=models.CharField(max_length=50)
+    storyLine=models.CharField(max_length=100)
+    platform=models.ForeignKey(StreamPlatform, on_delete=models.CASCADE)
+    active=models.BooleanField(default=True)
+    created=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
