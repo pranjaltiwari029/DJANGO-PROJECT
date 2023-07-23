a3a7de3b05cfd0dc0aeff862e55d04315f672afd
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-23 22:50:17 +0530

    Update admin.py

--- IMDB/imdb_api/admin.py
+++ IMDB/imdb_api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from . models import WatchList,StreamPLatform
+from .models  import StreamPlatform,WatchList
 
 # Register your models here.
 
-admin.site.register(StreamPLatform)
+admin.site.register(StreamPlatform)
 admin.site.register(WatchList)
