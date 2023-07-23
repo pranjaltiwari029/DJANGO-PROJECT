5db47a0e3104d08f7a50aad26d60254100e2d7ab
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-23 22:52:39 +0530

    Update views.py

--- IMDB/imdb_api/views.py
+++ IMDB/imdb_api/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import WatchList,StreamPlatform
 # Create your views here.
 
 def movie_list(request):
-    return HttpResponse("Testing list view")
+    movie_list=WatchList.objects.all()
+    print(movie_list)
+    return HttpResponse("testing movie list")
+
 
 def movie_detail(request,pk):
+    movie=WatchList.objects.get(pk=pk)
+    print(movie)
     
     return HttpResponse("testing movie detail view")
 
