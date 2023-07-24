3c26675cca673ac6577e021a0bc2d17f0d96d99b
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-24 15:01:39 +0530

    Update urls.py

--- IMDB/imdb_api/urls.py
+++ IMDB/imdb_api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     
     path('list/',views.movie_list,name="movie-list"),
-    path('list/<int:pk>',views.movie_detail,name='movie.detail'),
+    path('list/<int:pk>',views.movie_detail,name='movie-detail'),
+    path('stream/',views.stream_list,name='stream-platform'),
+    path('stream/<int:pk>',views.stream_detail,name='stream-platform-detail'),
 ]
