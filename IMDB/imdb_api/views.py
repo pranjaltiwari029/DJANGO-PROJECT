0a22ff45405072ad646a069ec2b7f3d8c54ecdfe
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-24 15:16:26 +0530

    Update views.py

--- IMDB/imdb_api/views.py
+++ IMDB/imdb_api/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse,JsonResponse
 from .models import WatchList,StreamPlatform
 from .serializers import WatchListSerializer,StreamPlatformSerializer
+from  rest_framework.response import Response
+from rest_framework import status
+
 # Create your views here.
 
 def movie_list(request):
@@ -17,10 +20,21 @@ def movie_detail(request,pk):
     return JsonResponse(serialized.data)
 
 def stream_list(request):
-    stream_list=StreamPlatform.objects.all()
-    serialized= StreamPlatformSerializer(stream_list,many= True)
-    return JsonResponse(serialized.data,safe=False)
-
+    if request.method=='GET':
+        stream_list=StreamPlatform.objects.all()
+        serialized= StreamPlatformSerializer(stream_list,many= True)
+        return JsonResponse(serialized.data,safe=False)
+    
+    elif request.method=='POST':
+        _data=request.data
+        serialized=StreamPlatformSerializer(data=_data)
+        if serialized.is_valid():
+            serialized.save()
+            return Response(serialized.data,status= status.HTTP_201_CREATED)
+        return Response(serialized.erros,status=status.HTTP_400_BAD_REQUEST)
+            
+            
+            
 def stream_detail(request,pk):
     stream_platform=StreamPlatform.objects.get(pk=pk)
     serialized=StreamPlatformSerializer(stream_platform)
