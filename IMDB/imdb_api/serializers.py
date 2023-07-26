19e774e7cb01e0451c5abb14b9eef75799163020
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-26 22:44:16 +0530

    Update serializers.py

--- IMDB/imdb_api/serializers.py
+++ IMDB/imdb_api/serializers.py
@@ -40,21 +40,21 @@ class StreamPlatformSerializer(serializers.Serializer):
 
 
 # class StreamPlatformSerializer(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    name=serializers.CharField(max_length=50)
-    about=serializers.CharField(max_length=100)
-    website=serializers.URLField(max_length=100)
+    # id=serializers.IntegerField(read_only=True)
+    # name=serializers.CharField(max_length=50)
+    # about=serializers.CharField(max_length=100)
+    # website=serializers.URLField(max_length=100)
     
-    def create(self, validated_data):
-        return StreamPlatform.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     return StreamPlatform.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
+    # def update(self, instance, validated_data):
         
-        # Update and return an existing `Watchlist` instance, given the validated data.
+    #     # Update and return an existing `Watchlist` instance, given the validated data.
         
-        instance.name = validated_data.get('name', instance.name)
-        instance.about = validated_data.get('about', instance.about)
-        instance.website = validated_data.get('website', instance.website)
-        instance.save()
-        return instance
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.about = validated_data.get('about', instance.about)
+    #     instance.website = validated_data.get('website', instance.website)
+    #     instance.save()
+    #     return instance
     
