3f2e342efc9fcd26c4dedc5173b51269565a705b
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-26 15:20:02 +0530

    Update urls.py

--- IMDB/imdb_api/urls.py
+++ IMDB/imdb_api/urls.py
@@ -5,10 +5,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     
-    path('list/',views.movie_list,name="movie-list"),
-    path('list/<int:pk>',views.movie_detail,name='movie-detail'),
-    path('stream/',views.stream_list,name='stream-platform'),
-    path('stream/<int:pk>',views.stream_detail,name='stream-platform-detail'),
+    # path('list/',views.movie_list,name="movie-list"),
+    # path('list/<int:pk>',views.movie_detail,name='movie-detail'),
+     path('stream/',views.StreamPlatformList.as_view(),name='stream-platform'),
+    path('stream/<int:pk>',views.StreamPlatformDetail.as_view(),name='stream-platform-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
