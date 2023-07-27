1a8b4875c0a857e9aef376bf4759b9d3896ffb1e
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-27 18:02:41 +0530

    Update settings.py

--- IMDB/IMDB/settings.py
+++ IMDB/IMDB/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
