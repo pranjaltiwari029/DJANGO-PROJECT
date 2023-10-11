a92997e16c76757f8647f7aaf1a3f38bfc950eb3
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-10-11 23:54:15 +0530

    cumtom user model

---
+++ UDEMY/foodOnline/accounts/signals.py
@@ -0,0 +1,37 @@
+from django.db.models.signals import post_save,pre_save
+from django.dispatch import receiver
+from .models import User,UserProfile
+
+@receiver(post_save,sender=User)
+def post_save_create_profile_receiver(sender,instance,created,**kwargs):
+    print(created)
+    if created:
+         
+         UserProfile.objects.create(user=instance)
+         print('user profile is created')
+    
+    else:
+        try:
+            profile=UserProfile.objects.get(user=instance)
+            profile.save()
+        except:
+            # create the user profile if not exists
+            UserProfile.objects.create(user=instance)
+            print('Profile was not present, but I created one')
+        print('user is updated')
+ 
+@receiver(pre_save,sender=User)       
+def pre_save_create_profile_receiver(sender,instance,**kwargs):
+    print(instance.username,'this user is being saved')
+    
+    
+         
+        
+    
+    
+
+# post_save.connect(post_save_create_profile_receiver,sender=User)
+     
+     
+     
+
--- UDEMY/foodOnline/foodOnline_main/settings.py
+++ UDEMY/foodOnline/foodOnline_main/settings.py
@@ -32,12 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # "admin_view_permission",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +86,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL='accounts.user'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -119,11 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT=BASE_DIR/'static'
 STATICFILES_DIRS=[
     'foodOnline_main/static'
 ]
+
+# Media Files Configuration
+MEDIA_URL='/media/'
+MEDIA_ROOT= BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
